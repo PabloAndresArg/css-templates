badd8ed254a331525d8822fa39df1fbd30c2cc29
pabloandresarg <pabloandresarg@gmail.com>
2022-07-15 17:52:32 -0600

    finish view techNews

--- Noticias Fazt/main.js
+++ Noticias Fazt/main.js
@@ -2,3 +2,8 @@ document.querySelector('.menu-btn').addEventListener('click' , () =>{
     // agregamos la clase show o quitamos si ya la tiene
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
