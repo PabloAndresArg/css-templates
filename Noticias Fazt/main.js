4ce0222413979135e96ed7ca7ec1de5b0e7cda04
pabloandresarg <pabloandresarg@gmail.com>
2022-07-15 14:50:10 -0600

    responsive

---
+++ Noticias Fazt/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-btn').addEventListener('click' , () =>{
+    // agregamos la clase show o quitamos si ya la tiene
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
